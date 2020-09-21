acf9707e09a313b5772f0c21f7c31e64fba7c63c
Tag_ <zucamarabu@gmail.com>
2020-09-21 10:43:50 -0300

    Updated .js

--- content.js
+++
@@ -1,6 +0,0 @@
-var password = document.getElementById("password")
-var username = document.getElementById("username")
-function saveData(){
-    localStorage.setItem("username", username.value)
-    localStorage.setItem("password", password.value)
-}
